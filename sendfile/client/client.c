9b1e579902c47f9e9401e8baf33b6a4ea60066e8
Sim-szm <xianszm007@163.com>
2014-06-07 16:51:33 +0800

    client- for easy

--- sendfile/client/client.c
+++ sendfile/client/client.c
@@ -3,7 +3,7 @@
  *
  *       Filename:  client.c
  *        Version:  1.0
- *        Created:  2014年06月07日 16时29分59秒
+ *        Created:  2014年06月07日 16时31分14秒
  *       Revision:  none
  *       Compiler:  clang
  *         Author:  sim szm, xianszm007@gmail.com
@@ -11,15 +11,51 @@
  *
  * =====================================================================================
  */
-
-#include <unistd.h>
-#include <assert.h>
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#define PORT 9527
+int main( int argc, char *argv[] ){
 
-int main(int argc, char *argv[])
-{
+	int sockfd;
+	char SEND_BUF[100]={"hello server , I'm login in !"};
+	char RECV_BUF[2048];
+	struct sockaddr_in server;
+	const char* file_name=argv[1];
 
-	return EXIT_SUCCESS;
+	int file_ptr=open(file_name,O_RDWR|O_CREAT,0666);
+	server.sin_family=AF_INET;
+	server.sin_addr.s_addr=htonl(INADDR_ANY);
+	server.sin_port=htons(PORT);
+
+	sockfd=socket(AF_INET,SOCK_STREAM,0);
+	if(connect(sockfd,(struct sockaddr *)&server,sizeof(server))==-1){
+		perror("connect error");
+		exit(1);
+	}
+
+	int ret=send(sockfd,SEND_BUF,100,0);
+
+	while(1){
+		ret=recv(sockfd,RECV_BUF,2048,0);
+		if(ret<0){
+			break;
+		}else if(ret==0){
+			break;
+		}else{
+			write(file_ptr,RECV_BUF,ret);
+			bzero(RECV_BUF,sizeof(RECV_BUF));
+		}
+	}
+	close(file_ptr);
+	close(sockfd);
+	return 0;
 }
-
