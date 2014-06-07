bbf34c018b19c143374b5b1dc9df615658eb6616
Sim-szm <xianszm007@163.com>
2014-06-07 16:30:33 +0800

    ...

---
+++ sendfile/client/client.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  client.c
+ *        Version:  1.0
+ *        Created:  2014年06月07日 16时29分59秒
+ *       Revision:  none
+ *       Compiler:  clang
+ *         Author:  sim szm, xianszm007@gmail.com
+ *        Company:  Class 1107 of Computer Science and Technology
+ *
+ * =====================================================================================
+ */
+
+#include <unistd.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+
+	return EXIT_SUCCESS;
+}
+
