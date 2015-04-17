1c5537039ed8277e96585ea2efa3a924707eb6f0
zhaomeng.szm <zhaomeng.szm@alibaba-inc.com>
2015-04-17 20:57:34 +0800

    data interface

---
+++ code-lab/src/com/threadpool/Impl/AbstractService.java
@@ -0,0 +1,8 @@
+package com.threadpool.Impl;
+
+/**
+ * Created by wenyun on 2015-04-16 20:03.
+ */
+public abstract  class AbstractService implements ResultContent {
+
+}
---
+++ code-lab/src/com/threadpool/Impl/ResultContent.java
@@ -0,0 +1,13 @@
+package com.threadpool.Impl;
+
+import com.threadpool.Model.DataModel;
+import com.threadpool.Model.ResultModel;
+
+/**
+ * Created by wenyun on 2015-04-16 19:49.
+ */
+public interface ResultContent {
+
+    public ResultModel resultBack(DataModel dataModel);
+
+}
